7cdd43107bdefa3de7c36f3065c02fbce368add9
Ngoc Dao <ngocdaothanh@gmail.com>
2012-02-23 21:44:37 +0900

    Add comment about DataLoader.Load

--- loader/DataLoader.cs
+++ loader/DataLoader.cs
@@ -1,6 +1,18 @@
 using System;
 using System.Reflection;
 
+/*
+The class should look like this:
+
+public class Level123 {
+    public static byte[] data =
+    {
+        0x53, 0x65, 0x65
+    };
+}
+
+=> byte[] mydata = DataLoader.Load("Level123");
+*/
 public class DataLoader
 {
 	public static byte[] Load(string className)
